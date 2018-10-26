d1209b63a0f89c5545eb3516704a9d945669ed50
Alex Burkowsky <aburkowsky9@gmail.com>
2018-10-25 18:24:07 -0700

    seed table course

--- Db/index.js
+++ Db/index.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const Promise = require('bluebird');
+const seedArray = require('./seed.js');
 
 const connection = mysql.createConnection({
   user: 'root',
@@ -11,50 +12,60 @@ const createTables = () => {
   // creates course table
   return db.queryAsync(`
     CREATE TABLE IF NOT EXISTS Course (
-      id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-      title VARCHAR(50) NULL DEFAULT NULL,
-      description VARCHAR(150) NULL DEFAULT NULL,
-      tag VARCHAR(13) NULL DEFAULT NULL,
-      avg_rating DECIMAL(2, 1) NULL DEFAULT NULL,
-      total_ratings INTEGER(5) NULL DEFAULT NULL,
-      enrollment INTEGER(5) NULL DEFAULT NULL,
-      created_by VARCHAR(25) NULL DEFAULT NULL,
-      last_updated TIMESTAMP NULL DEFAULT NULL,
-      language VARCHAR(15) NULL DEFAULT NULL,
-      img_url VARCHAR(50) NULL DEFAULT NULL,
-      list_price VARCHAR(7) NULL DEFAULT NULL,
-      discount_price VARCHAR(7) NULL DEFAULT NULL,
-      video_hrs DECIMAL(3, 1) NULL DEFAULT NULL,
-      total_articles INTEGER(3) NULL DEFAULT NULL,
-      total_downloads INTEGER(3) NULL DEFAULT NULL,
-      active_coupon VARCHAR(10) NULL DEFAULT NULL,
-      PRIMARY KEY (id)
+      id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      title VARCHAR(70),
+      description VARCHAR(400),
+      tag VARCHAR(13),
+      avg_rating DECIMAL(2, 1),
+      total_ratings INTEGER(3),
+      enrollment INTEGER(3),
+      created_by VARCHAR(25),
+      last_updated VARCHAR(7),
+      language VARCHAR(25),
+      img_url VARCHAR(100),
+      list_price VARCHAR(7),
+      discount_price VARCHAR(7),
+      video_hrs DECIMAL(3, 1),
+      total_articles INTEGER(3),
+      total_downloads INTEGER(3),
+      active_coupon VARCHAR(11)
     );`)
     // creates CC table
     .then(() => {
       db.queryAsync(`
-      CREATE TABLE IF NOT EXISTS CC (
-        id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-        option VARCHAR(15) NULL DEFAULT NULL,
-        PRIMARY KEY (id)
+        CREATE TABLE IF NOT EXISTS CC (
+          id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          cc_option VARCHAR(15)
       );`);
     })
     .then(() => {
       db.queryAsync(`
-      CREATE TABLE Course_CC (
-        course_id INTEGER NULL DEFAULT NULL,
-        cc_id INTEGER NULL DEFAULT NULL
-        FOREIGN KEY (course_id) 
-          REFERENCES Course (id)
-          ON DELETE CASCADE ON UPDATE CASCADE,
-        FOREIGN KEY (cc_id) 
-          REFERENCES CC (id)
-          ON DELETE CASCADE ON UPDATE CASCADE
+        CREATE TABLE IF NOT EXISTS Course_CC (
+          course_id INTEGER,
+          cc_id INTEGER     
       );`);
+    })
+    .then(() => {
+      db.queryAsync(`
+        ALTER TABLE Course_CC ADD FOREIGN KEY (course_id) REFERENCES Course (id);
+      `);
+    })
+    .then(() => {
+      db.queryAsync(`
+        ALTER TABLE Course_CC ADD FOREIGN KEY (cc_id) REFERENCES Course (id);
+      `);
     });
+};
+
+const createData = () => {
+  const queryStr = 'INSERT INTO Course SET ?';
+  for (let i = 0; i < seedArray.length; i += 1) {
+    db.queryAsync(queryStr, seedArray[i]);
+  }
 };
 
 db.queryAsync('CREATE DATABASE IF NOT EXISTS headerSidebar')
   .then(() => console.log(`Connected to CheckoutData database as ID ${db.threadId}`))
   .then(() => db.queryAsync('USE headerSidebar'))
-  .then(() => createTables(db));
+  .then(() => createTables(db))
+  .then(() => createData());
