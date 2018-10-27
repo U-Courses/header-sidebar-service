eff431906a9f4e4236299bee2ec5a78f3d09e9a3
Alex Burkowsky <aburkowsky9@gmail.com>
2018-10-26 19:31:13 -0700

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'client/src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.(js|jsx)$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+          },
+        },
+      },
+      {
+        test: [/\.scss$/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
