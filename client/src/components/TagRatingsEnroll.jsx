1a4e6e23d27715c4e13e605bc419f6f1dafa8e05
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-01 15:04:02 -0700

    Add margin if tag present, do not add if tag is null

--- client/src/components/TagRatingsEnroll.jsx
+++ client/src/components/TagRatingsEnroll.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const TagRatingsEnroll = ({ course }) => (
-  <section className="tag-ratings-enroll">
-    <div className="tag">{ course.tag }</div>
-    <div className="ratings">
-      <span className="stars">Stars</span>
-      <span className="avg-rating">{ course.avg_rating }</span>
-      <span className="total-ratings">{ `(${course.total_ratings} ratings)` }</span>
-    </div>
-    <div className="enrollment">{ `${course.enrollment} students enrolled` }</div>
-  </section>
-);
+const TagRatingsEnroll = ({ course }) => {
+  let tagMargin;
+  if (course.tag) {
+    tagMargin = {'margin-right': '15px'};
+  } else {
+    tagMargin = {};
+  }
+  return(
+    <section className="tag-ratings-enroll">
+      <div className="tag" style={ tagMargin }>{ course.tag }</div>
+      <div className="ratings">
+        <span className="stars">Stars</span>
+        <span className="avg-rating">{ course.avg_rating }</span>
+        <span className="total-ratings">{ `(${course.total_ratings} ratings)` }</span>
+      </div>
+      <div className="enrollment">{ `${course.enrollment} students enrolled` }</div>
+    </section>
+  )
+};
 
 export default TagRatingsEnroll;
