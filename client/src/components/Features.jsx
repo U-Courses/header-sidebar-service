f9dd591de0a507fe2077a24698918564a11a081b
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-02 23:01:28 -0700

    Add icons to features section

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 const Features = ({ video_hrs, total_articles }) => (
   <section className="features">
@@ -7,11 +10,36 @@ const Features = ({ video_hrs, total_articles }) => (
     </div>
     <div>
       <ul>
-        <li><span className="feature-icon"></span><span className="feature-text">{ `${video_hrs} hours on-demand video` }</span></li>
-        <li><span className="feature-icon"></span><span className="feature-text">{ `${total_articles} ${articleOrArticles(total_articles)}` }</span></li>
-        <li><span className="feature-icon"></span><span className="feature-text">Full Lifetime Access</span></li>
-        <li><span className="feature-icon"></span><span className="feature-text">Access on mobile and TV</span></li>
-        <li><span className="feature-icon"></span><span className="feature-text">Certificate of Completion</span></li>
+        <li>
+          <span className="feature-icon">
+            <FontAwesomeIcon icon = { far.faFileVideo }/>
+          </span>
+          <span className="feature-text">{ `${video_hrs} hours on-demand video` }</span>
+        </li>
+        <li>
+          <span className="feature-icon">
+            <FontAwesomeIcon icon = { far.faFile }/>
+          </span>
+          <span className="feature-text">{ `${total_articles} ${articleOrArticles(total_articles)}` }</span>
+        </li>
+        <li>
+          <span className="feature-icon">
+            <FontAwesomeIcon icon = { fas.faInfinity }/>
+          </span>
+          <span className="feature-text">Full Lifetime Access</span>
+        </li>
+        <li>
+          <span className="feature-icon">
+            <FontAwesomeIcon icon = { fas.faMobileAlt }/>
+          </span>
+          <span className="feature-text">Access on mobile and TV</span>
+        </li>
+        <li>
+          <span className="feature-icon">
+            <FontAwesomeIcon icon = { fas.faAtom }/>
+          </span>
+          <span className="feature-text">Certificate of Completion</span>
+        </li>
       </ul>
     </div>
   </section>
