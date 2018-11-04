07d19450d30698d57fc50e35977daf95dd43d0cf
Alex Burkowsky <aburkowsky9@gmail.com>
2018-11-04 13:13:00 -0800

    add fixedHeader component, set opacity and z-index on fixedHeader and sidebar component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header.jsx';
+import FixedHeader from './FixedHeader.jsx';
 import Sidebar from './Sidebar.jsx';
 import requests from '../lib/requests.js';
 import TopRow from './TopRow.jsx';
@@ -11,10 +12,14 @@ class App extends React.Component {
     this.state = {
       courseId: 97,
       courseData: {},
+      headerFixed: false,
     };
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+
     requests.getCourseData(this.state.courseId)
       .then(data => (
         this.setState({
@@ -22,6 +27,15 @@ class App extends React.Component {
           courseData: data,
         })
       ));
+  }
+
+  handleScroll() {
+    const bannerHeight = document.querySelector('.App__banner___3EFF9').offsetHeight;
+    if ((!this.state.headerFixed) && (window.scrollY >= bannerHeight)) {
+      this.setState({ headerFixed: true });
+    } else if (this.state.headerFixed && (window.scrollY <= bannerHeight)) {
+      this.setState({ headerFixed: false });
+    }
   }
 
   render() {
@@ -36,6 +50,10 @@ class App extends React.Component {
             </div>
           </div>
         </div>
+        {this.state.headerFixed
+          ? <FixedHeader course={ this.state.courseData } />
+          : null
+        }
         <div className={ styles.belowContent }>Remove after Scroll Implementation</div>
       </div>
     );
